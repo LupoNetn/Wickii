5636e58f57ac362406e91cedfb1217ad73cb588f
lupo tobi <daniellupo30@gmail.com>
2025-04-29 21:25:50 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { BrowserRouter,Routes,Route} from 'react-router-dom'
+
+const App:React.FC = () => {
+  return (
+   
+    <div>
+      
+    </div>
+  
+  )
+}
+
+export default App
