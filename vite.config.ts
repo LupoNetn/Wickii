f80c63fca8cadc0698a38145ac0c435cc1973278
lupo tobi <daniellupo30@gmail.com>
2025-05-02 23:20:32 +0100

    fix: correct base path casing in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/wickii/', // <-- your repo name
+  base: '/Wickii/', // <-- your repo name
 })
